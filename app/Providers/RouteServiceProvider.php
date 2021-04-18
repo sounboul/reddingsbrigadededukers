f98ea1676ddfa1b236be91b57d6196dd1c097d3d
harmjanbeekhuis <harmjanbeekhuis@gmail.com>
2021-04-18 14:50:20 +0200

    force https

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -5,8 +5,10 @@ namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -35,6 +37,11 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        /* Force https */
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
+
         $this->configureRateLimiting();
 
         $this->routes(function () {
